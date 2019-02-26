cb3944bfc3487a03e270c5714e70bb57d6da8bf9
fabiocuri <fcuri91@gmail.com>
2019-02-26 17:57:12 +0000

    Delete BPE.sh

--- BPE.sh
+++
@@ -1,9 +0,0 @@
-python subword-nmt-master/learn_bpe.py -s 10000 < TRI/simulations/simulation_IA_preprocessed.txt > TRI/simulations/simulation_IA_preprocessed.BPE
-python subword-nmt-master/learn_bpe.py -s 10000 < TRI/simulations/simulation_IB_preprocessed.txt > TRI/simulations/simulation_IB_preprocessed.BPE
-python subword-nmt-master/learn_bpe.py -s 10000 < TRI/simulations/simulation_IIA_preprocessed.txt > TRI/simulations/simulation_IIA_preprocessed.BPE
-python subword-nmt-master/learn_bpe.py -s 10000 < TRI/simulations/simulation_IIB_preprocessed.txt > TRI/simulations/simulation_IIB_preprocessed.BPE
-
-python subword-nmt-master/apply_bpe.py -c TRI/simulations/simulation_IA_preprocessed.BPE < TRI/simulations/simulation_IA_preprocessed.txt > TRI/simulations/simulation_IA_preprocessed_BPE.txt
-python subword-nmt-master/apply_bpe.py -c TRI/simulations/simulation_IB_preprocessed.BPE < TRI/simulations/simulation_IB_preprocessed.txt > TRI/simulations/simulation_IB_preprocessed_BPE.txt
-python subword-nmt-master/apply_bpe.py -c TRI/simulations/simulation_IIA_preprocessed.BPE < TRI/simulations/simulation_IIA_preprocessed.txt > TRI/simulations/simulation_IIA_preprocessed_BPE.txt
-python subword-nmt-master/apply_bpe.py -c TRI/simulations/simulation_IIB_preprocessed.BPE < TRI/simulations/simulation_IIB_preprocessed.txt > TRI/simulations/simulation_IIB_preprocessed_BPE.txt
