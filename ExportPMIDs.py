08b703704fdab753152d16572151629df01d0c0b
fabiocuri <fcuri91@gmail.com>
2019-03-04 15:25:58 +0000

    Update ExportPMIDs.py

--- ExportPMIDs.py
+++ ExportPMIDs.py
@@ -11,12 +11,29 @@ def list_as_txt(l, l_name):
     with open(l_name, 'w') as f:
         for item in l:
             f.write("%s\n" % item)
+            
+def read_txt(l):
+
+    ''' Read txt as list '''
+
+    l_ = []
+    with open(l, "rt") as f:
+        l_ = f.read().splitlines()
+
+    return l_
 
 if '__main__' == __name__:
     
     cwd = os.getcwd()
-    df = pd.read_csv(cwd + '/abstracts.all.labeled.csv', sep='\n|\t', encoding='utf-8', engine='python')
-    training_pmid = list(df.pmid)
-
-    list_as_txt(training_pmid, 'training_pmid.txt')
-
+    
+    ann1 = pd.read_csv(cwd + '/abstracts.all.labeled.csv', sep='\n|\t', encoding='utf-8', engine='python')
+    ann2 = read_txt(cwd + '/Gold_Label_train.tsv')
+    ann3 = read_txt(cwd + '/Gold_Label_test.tsv')
+    
+    ann2 = [x.split('\t') for x in ann2]
+    ann3 = [x.split('\t') for x in ann3]
+    
+    list_PMID = list(ann1['pmid']) + [int(x[0]) for x in ann2] + [int(x[0]) for x in ann3]
+    list_PMID = list(set(list_PMID))
+    
+    list_as_txt(list_PMID, 'list_PMID.txt')
