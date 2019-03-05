6c034dacd2cfd8bfeb3c8c21eb1d247548a068ac
fabiocuri <fcuri91@gmail.com>
2019-03-05 14:14:21 +0000

    Update ExportPMIDs.py

--- ExportPMIDs.py
+++ ExportPMIDs.py
@@ -1,1 +1,42 @@
 
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import pandas as pd
+
+def read_as_list(l, encoding):
+
+    ''' Read file as list '''
+
+    l_ = []
+    with open(l, "rt", encoding=encoding) as f:
+        l_ = f.read().splitlines()
+
+    return l_
+
+def write_list(l, l_name, iterate, encoding):
+
+    ''' Export list '''
+
+    if iterate:
+        with open(l_name, 'w', encoding=encoding) as f:
+            for item in l:
+                f.write("%s\n" % item)
+    else:
+        with open(l_name, 'w', encoding=encoding) as f:
+            f.write("%s\n" % l)
+
+if '__main__' == __name__:
+    
+    cwd = os.getcwd()
+    encoding = "latin-1"
+    
+    ann1 = pd.read_csv(cwd + '/abstracts.all.labeled.csv', sep='\n|\t', encoding=encoding, engine='python')
+    ann2 = read_as_list(cwd + '/Gold_Label_train.tsv', encoding=encoding)
+    ann2 = [x.split('\t') for x in ann2]
+    ann3 = read_as_list(cwd + '/Gold_Label_test.tsv', encoding=encoding)
+    ann3 = [x.split('\t') for x in ann3]
+    
+    list_PMID = list(set(list(ann1['pmid']) + [int(x[0]) for x in ann2] + [int(x[0]) for x in ann3]))
+    write_list(list_PMID, 'list_PMID.txt', iterate=True, encoding=encoding)
