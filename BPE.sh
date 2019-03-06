e1eff03917fe944911c9e29e6fc5c2db06002e39
fabiocuri <fcuri91@gmail.com>
2019-03-06 10:23:46 +0000

    Update BPE.sh

--- BPE.sh
+++ BPE.sh
@@ -4,3 +4,5 @@ for NAME in ${DATA[@]}; do
 
     python ../subword-nmt-master/learn_bpe.py -s 10000 < simulations/${NAME}.txt > simulations/${NAME}.BPE
     python ../subword-nmt-master/apply_bpe.py -c simulations/${NAME}.BPE < simulations/${NAME}.txt > simulations/${NAME}_BPE.txt
+
+done
