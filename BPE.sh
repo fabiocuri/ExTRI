1e84000eae640eedd51b24df7a6d699e354d1a8c
fabiocuri <fcuri91@gmail.com>
2019-03-07 15:29:30 +0000

    Delete BPE.sh

--- BPE.sh
+++
@@ -1,8 +0,0 @@
-DATA=("IA" "IB" "IIA" "IIB" "IA_F" "IB_F" "IIA_F" "IIB_F")
-
-for NAME in ${DATA[@]}; do
-
-    python ../subword-nmt-master/learn_bpe.py -s 10000 < simulations/${NAME}.txt > simulations/${NAME}.BPE
-    python ../subword-nmt-master/apply_bpe.py -c simulations/${NAME}.BPE < simulations/${NAME}.txt > simulations/${NAME}_BPE.txt
-
-done
