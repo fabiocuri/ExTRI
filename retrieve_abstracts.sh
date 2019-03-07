b96fd05a9e608a226991e576614ba7773573195d
fabiocuri <fcuri91@gmail.com>
2019-03-07 15:32:49 +0000

    Delete retrieve_abstracts.sh

--- retrieve_abstracts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-filename='../ExTRI-master/list_pmid.txt'
-filelines=`cat $filename`
-echo Start
-for line in $filelines ; do
-    perl PreProcessing.pl -t PMID -i $line -o input/$line.txt
-done
