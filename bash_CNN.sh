62633da7f738ba0047aa1786d39e453c36cfb00f
fabiocuri <fcuri91@gmail.com>
2019-03-13 16:25:55 +0000

    Update bash_CNN.sh

--- bash_CNN.sh
+++ bash_CNN.sh
@@ -1,16 +1,17 @@
-DATA=("IA_F" "IB_F" "IIA_F" "IIB_F" "IA_F_BPE" "IB_F_BPE" "IIA_F_BPE" "IIB_F_BPE")
+DATA=("IB_F" "IIB_F" "IB_F_BPE" "IIB_F_BPE")
 MAX_NUM_WORDS=("500" "20000")
 DIM_CONV=("64" "128" "256")
 OPTIMIZER=("adam" "rmsprop")
+OVERSAMPLING=("SMOTE" "ROS" "ADASYN")
+MODEL_SELECTION="yes"
 
 for i in ${DATA[@]}; do
     for j in ${MAX_NUM_WORDS[@]}; do
         for k in ${DIM_CONV[@]}; do
-                for o in ${OPTIMIZER[@]}; do
+            for o in ${OPTIMIZER[@]}; do
 
-                    python3 RunCNN.py --data ${i} --labels labels --max_num_words ${j} --dim_CONV ${k} --optimizer ${o}
+                    python3 RunCNN.py --data ${i} --labels labels --max_num_words ${j} --dim_CONV ${k} --optimizer ${o} --oversampling ${OVERSAMPLING} --model_selection ${MODEL_SELECTION} 
 
-                done
             done
         done
     done
