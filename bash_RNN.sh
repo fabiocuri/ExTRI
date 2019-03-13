087c098036887503bd77a9ae05b19665b7eaaeb4
fabiocuri <fcuri91@gmail.com>
2019-03-13 16:26:20 +0000

    Update bash_RNN.sh

--- bash_RNN.sh
+++ bash_RNN.sh
@@ -1,8 +1,10 @@
-DATA=("IA_F" "IB_F" "IIA_F" "IIB_F" "IA_F_BPE" "IB_F_BPE" "IIA_F_BPE" "IIB_F_BPE")
+DATA=("IB_F" "IIB_F" "IB_F_BPE" "IIB_F_BPE")
 MAX_NUM_WORDS=("500" "20000")
 DIM_LSTM=("100" "500")
 ATTENTION=("None" "Att")
 OPTIMIZER=("adam" "rmsprop")
+OVERSAMPLING=("SMOTE" "ROS" "ADASYN")
+MODEL_SELECTION="yes"
 
 for i in ${DATA[@]}; do
     for j in ${MAX_NUM_WORDS[@]}; do
@@ -10,11 +12,10 @@ for i in ${DATA[@]}; do
             for l in ${ATTENTION[@]}; do
                 for o in ${OPTIMIZER[@]}; do
 
-                    python3 RunRNN.py --data ${i} --labels labels --max_num_words ${j} --dim_LSTM ${k} --attention ${l} --optimizer ${o}
+                    python3 RunRNN.py --data ${i} --labels labels --max_num_words ${j} --dim_LSTM ${k} --attention ${l} --optimizer ${o} --oversampling ${OVERSAMPLING} --model_selection ${MODEL_SELECTION} 
 
                 done
             done
         done
     done
 done
-
