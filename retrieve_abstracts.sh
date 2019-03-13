1e3667b179cc4ae94661ebacfb6acfca8c5c0577
fabiocuri <fcuri91@gmail.com>
2019-03-13 16:07:00 +0000

    Update retrieve_abstracts.sh

--- retrieve_abstracts.sh
+++ retrieve_abstracts.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-filename='../ExTRI-master/list_pmid.txt'
+filename='list_PMID.txt'
 filelines=`cat $filename`
 echo Start
 for line in $filelines ; do
-    perl PreProcessing.pl -t PMID -i $line -o input/$line.txt
+    perl PreProcessing.pl -t PMID -i $line -o articles_pubtator/$line.txt
+done
+
+filename='list_PMID_test.txt'
+filelines=`cat $filename`
+echo Start
+for line in $filelines ; do
+    perl PreProcessing.pl -t PMID -i $line -o articles_pubtator_test/$line.txt
 done
