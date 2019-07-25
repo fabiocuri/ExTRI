951ef963c8ffa27339aa79c6b299db88fffd4bc6
fabiocuri <fcuri91@gmail.com>
2019-07-25 10:31:17 +0000

    Update retrieve_test.sh

--- retrieve_test.sh
+++ retrieve_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-filename_train=../RE/test.txt
+filename_train=test.txt
 filelines_train=`cat $filename_train`
 
 echo Start
+mkdir -p ./test/pubtator
 for line in $filelines_train ; do
-    perl PreProcessing.pl -t PMID -i $line -o ../RE/test/pubtator/$line.txt
+    perl PreProcessing.pl -t PMID -i $line -o ./test/pubtator/$line.txt
 done
