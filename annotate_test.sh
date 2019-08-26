2a9d85e2e70679ad926d527e4a85ab77d0534a1c
fabiocuri <fcuri91@gmail.com>
2019-08-26 14:40:22 +0000

    Update annotate_test.sh

--- annotate_test.sh
+++ annotate_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-INPUT=./test/pubtator
-mkdir -p ./test/GNormPlus
-mkdir -p ./test/text
-OUTPUT=./test/GNormPlus
+INPUT=../test/pubtator
+mkdir -p ../test/GNormPlus
+mkdir -p ../test/text
+OUTPUT=../test/GNormPlus
 SETUP=setup.txt
 
 java -Xmx20G -Xms20G  -jar GNormPlus.jar $INPUT $OUTPUT $SETUP
