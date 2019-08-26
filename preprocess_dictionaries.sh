64d8792515c1609356ee80bdaa70bd8b77d808f4
fabiocuri <fcuri91@gmail.com>
2019-08-26 14:03:53 +0000

    Update preprocess_dictionaries.sh

--- preprocess_dictionaries.sh
+++ preprocess_dictionaries.sh
@@ -1,9 +1,9 @@
 echo "Converting hyphens to underscores and removing punctuations"
-sed 's/-/ /g' nodbtf_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > nodbtf_official_underscore.dic.txt2
-sed 's/-/ /g' dbtf_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > dbtf_syn_underscore.dic.txt2
-sed 's/-/ /g' nodbtf_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > nodbtf_syn_underscore.dic.txt2
-sed 's/-/ /g' dbtf_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > dbtf_official_underscore.dic.txt2
-sed 's/-/ /g' nodbtf_long_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > nodbtf_long_syn_underscore.dic.txt2
-sed 's/-/ /g' nodbtf_long_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > nodbtf_long_official_underscore.dic.txt2
-sed 's/-/ /g' dbtf_long_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > dbtf_long_syn_underscore.dic.txt2
-sed 's/-/ /g' dbtf_long_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > dbtf_long_official_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/nodbtf_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/nodbtf_official_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/dbtf_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/dbtf_syn_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/nodbtf_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/nodbtf_syn_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/dbtf_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/dbtf_official_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/nodbtf_long_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/nodbtf_long_syn_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/nodbtf_long_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/nodbtf_long_official_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/dbtf_long_syn_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/dbtf_long_syn_underscore.dic.txt2
+sed 's/-/ /g' ./dictionaries/dbtf_long_official_underscore.dic | sed 's/_/ /g' | sed 's/\./ /g' | sed 's/\// /g' | sed 's/)/ /g' | sed 's/(/ /g' > ./dictionaries/dbtf_long_official_underscore.dic.txt2
