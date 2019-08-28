2e337bed840e779c62bafb8f0d83b784ad1f016b
fabiocuri <fcuri91@gmail.com>
2019-08-28 09:17:04 +0000

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+import re
+import string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from spacy.lang.en.stop_words import STOP_WORDS
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from textblob import TextBlob
+
+def preprocess(text):
+
+    text = text.lower() # Lowercase
+    text = re.sub(r'\d+', '', text) # Remove integers
+    text = text.strip() # Remove white spaces
+    
+    stop_words = list(set(list(stopwords.words('english')) + list(ENGLISH_STOP_WORDS) + list(STOP_WORDS))) # Remove stopwords
+    stop_words = list(set(stop_words))
+    tokens = word_tokenize(text)
+    text = [i for i in tokens if not i in stop_words]
+
+    stemmer= PorterStemmer()
+
+    text = [stemmer.stem(word) for word in text] # Stemming
+    text = ' '.join(text)
+
+    return text
