0d027351d7eab41b9b66f713252448fd35e32e72
fabiocuri <fcuri91@gmail.com>
2019-09-02 14:53:28 +0000

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -18,7 +18,7 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
 from collections import defaultdict
 from nltk.corpus import wordnet as wn
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score
 from export_abstracts import read_as_list, write_list
@@ -32,28 +32,40 @@ from spacy.lang.en.stop_words import STOP_WORDS
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob
+from scipy.sparse import hstack
+from RF import build_features
 
-def run_SVM(X, test, labels):
+def run_SVM(X, test, labels, data, f):
 
     np.random.seed(500)
 
     Encoder = LabelEncoder()
     labels = Encoder.fit_transform(labels)
 
-    Tfidf_vect = TfidfVectorizer(max_features=5000)
-    Tfidf_vect.fit(X)
-    Train_X_Tfidf = Tfidf_vect.transform(X)
-    Test_X_Tfidf = Tfidf_vect.transform(test)
+    if f == 'TF-IDF':
+        vect = TfidfVectorizer(max_features=1000)
+    if f == 'BoW':
+        vect = CountVectorizer(max_features=1000)
+
+    vect.fit(X)
+    Train_X_Tfidf = vect.transform(X)
+    Test_X_Tfidf = vect.transform(test)
+
+    features_train = build_features(X)
+    features_test = build_features(test)
+
+    train = hstack((Train_X_Tfidf, features_train))
+    test = hstack((Test_X_Tfidf, features_test))
 
     SVM_linear = svm.SVC(C=1.0, kernel='linear', degree=1, gamma='auto')
     SVM_linear.fit(Train_X_Tfidf,labels)
 
     y_pred = SVM_linear.predict(Test_X_Tfidf)
-    write_list(y_pred, cwd + '/models/predictions.txt', iterate=True, encoding=encoding)
+    write_list(y_pred, cwd + '/models/SVM_predictions' + data + '_' + f + '.txt', iterate=True, encoding=encoding)
 
 if '__main__' == __name__:
 
-    ''' Run Recurrent Neural Network '''
+    ''' Run SVM '''
 
     encoding = 'latin-1'
     cwd = os.getcwd()
@@ -61,7 +73,13 @@ if '__main__' == __name__:
     df = pd.read_csv('./train_data.csv')
     labels = list(df['tags'])
 
-    train = read_as_list('./train_preprocessed.txt', encoding='latin-1')
-    test = read_as_list('./test_preprocessed.txt', encoding='latin-1')
+    datasets = ['_preprocessed', '_preprocessed_BPE', '_preprocessed_shortest', '_preprocessed_shortest_BPE']
+    features = ['TF-IDF', 'BoW']
 
-    run_SVM(train, test, labels)
+    for data in datasets:
+        for f in features:
+
+            train = read_as_list('./data/train' + data + '.txt', encoding='latin-1')
+            test = read_as_list('./data/test' + data + '.txt', encoding='latin-1')
+
+            run_SVM(train, test, labels, data, f)
