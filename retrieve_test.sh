14ec028b64a0c4d70fe99f9bef7a24e41dfa008b
fabiocuri <fcuri91@gmail.com>
2019-09-27 13:24:09 +0000

    Delete retrieve_test.sh

--- retrieve_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-filename_train=test.txt
-filelines_train=`cat $filename_train`
-
-echo Start
-mkdir -p ../test/pubtator
-for line in $filelines_train ; do
-    perl PreProcessing.pl -t PMID -i $line -o ../test/pubtator/$line.txt
-done
