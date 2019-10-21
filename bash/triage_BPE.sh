0258e730d16acbf73a6699ee758f6899b53ade48
fabiocuri <fcuri91@gmail.com>
2019-10-21 13:52:50 +0000

    Delete triage_BPE.sh

--- bash/triage_BPE.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DATA=("triage_data_preprocessed_train" "triage_data_original_train")
-
-for NAME in ${DATA[@]}; do
-
-    python learn_bpe.py -s 10000 < ${NAME}.txt > ${NAME}.BPE
-    python apply_bpe.py -c ${NAME}.BPE < ${NAME}.txt > ${NAME}_BPE.txt
-
-done
