5809d07cfb811203514f8080d79827c037edde0a
fabiocuri <fcuri91@gmail.com>
2019-10-21 13:51:49 +0000

    Delete annotate_train_triage.sh

--- bash/annotate_train_triage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-INPUT=../train_triage/pubtator
-mkdir -p ../train_triage/GNormPlus
-mkdir -p ../train_triage/text
-OUTPUT=../train_triage/GNormPlus
-SETUP=setup.txt
-
-java -Xmx20G -Xms20G  -jar GNormPlus.jar $INPUT $OUTPUT $SETUP
