094e68cd6dd30270930b3053cceb52eaedd24861
fabiocuri <fcuri91@gmail.com>
2019-10-21 13:51:44 +0000

    Delete annotate_test.sh

--- bash/annotate_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-INPUT=../test/pubtator
-mkdir -p ../test/GNormPlus
-mkdir -p ../test/text
-OUTPUT=../test/GNormPlus
-SETUP=setup.txt
-
-java -Xmx20G -Xms20G  -jar GNormPlus.jar $INPUT $OUTPUT $SETUP
